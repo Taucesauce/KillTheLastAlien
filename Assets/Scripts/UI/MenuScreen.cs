87d9e7ebf188c0a10de3dbe0afa7c1b8b8fdbfb6
Justin Terry <justin.terry@loop.colum.edu>
2017-06-17 00:43:48 -0500

    Reorganized scripts, added menu assets.

---
+++ Assets/Scripts/UI/MenuScreen.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuScreen : MonoBehaviour {
+    [SerializeField]
+    Sprite[] menuScreens = new Sprite[5];
+
+    Image currentImage;
+    int screenIndex = 0;
+
+	// Use this for initialization
+	void Start () {
+        currentImage = GetComponent<Image>();
+	}
+
+    void PrevScreen() {
+        if (screenIndex < menuScreens.Length) {
+            screenIndex--;
+            currentImage.sprite = menuScreens[screenIndex];
+        }
+    }
+
+    void NextScreen() {
+        if(screenIndex < menuScreens.Length) {
+            screenIndex++;
+            currentImage.sprite = menuScreens[screenIndex];
+        }
+    }
+}
